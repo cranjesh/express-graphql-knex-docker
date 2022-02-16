3c2d01fc27dde1ae57d3e81bed45704f9cc4f92c
cranjesh <63446800+cranjesh@users.noreply.github.com>
2022-02-16 18:05:52 +1100

    Update index.ts

--- src/grpc/service/index.ts
+++ src/grpc/service/index.ts
@@ -1,43 +1,33 @@
-import {
-    handleUnaryCall,
-    status as grpcStatus
-} from "@grpc/grpc-js";
+import { handleUnaryCall, status as grpcStatus } from '@grpc/grpc-js';
 
-import {
-    VerifyReq,
-    VerifyResp
-} from "../stub/token_auth_pb";
+import { VerifyReq, VerifyResp } from '../stub/token_auth_pb';
 
-import {
-    TokenAuthService,
-    ITokenAuthServer
-} from "../stub/token_auth_grpc_pb";
+import { TokenAuthService, ITokenAuthServer } from '../stub/token_auth_grpc_pb';
 
 import * as tokenServ from '../../service/token';
 
 class TokenAuthServer implements ITokenAuthServer {
-    [name: string]: import("@grpc/grpc-js").UntypedHandleCall;
-    verifyAccessToken: handleUnaryCall<VerifyReq, VerifyResp> = async (
-        call,
-        callback,
-    ) => {
-        const { request } = call; // get request object
-        // validate request | middleware >>>
-        let invalid = false;
-        if (invalid) {
-            return callback({
-                code: grpcStatus.INVALID_ARGUMENT,
-                name: "Invalid Request",
-                message: "Bad request: the argument was not provided",
-            });
-        }
-        // <<< validate request | middleware
-        const response: VerifyResp = new VerifyResp();
-        const token = request.getToken();
-        const valid = await tokenServ.verifyAccessToken(token);
-        response.setResult(valid);
-        callback(null, response);
-    };
-};
+  [name: string]: import('@grpc/grpc-js').UntypedHandleCall;
+  verifyAccessToken: handleUnaryCall<VerifyReq, VerifyResp> = async (call, callback) => {
+    console.log('gRPC TokenAuthServer verifyAccessToken invoked');
+    const { request } = call; // get request object
+    // validate request | middleware >>>
+    const invalid = false;
+    if (invalid) {
+      return callback({
+        code: grpcStatus.INVALID_ARGUMENT,
+        name: 'Invalid Request',
+        message: 'Bad request: the argument was not provided'
+      });
+    }
+    // <<< validate request | middleware
+    const response: VerifyResp = new VerifyResp();
+    const token = request.getToken();
+    const valid = await tokenServ.verifyAccessToken(token);
+    response.setResult(valid);
+    console.log('gRPC TokenAuthServer verifyAccessToken responded');
+    callback(null, response);
+  };
+}
 
-export { TokenAuthService, TokenAuthServer }
+export { TokenAuthService, TokenAuthServer };
